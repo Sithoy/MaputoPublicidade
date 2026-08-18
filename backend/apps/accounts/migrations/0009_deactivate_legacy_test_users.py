714b6fd3fe71ee1d466cdd54d9a2b072ae4793ee
Osvaldo Sitoe <114060186+Sithoy@users.noreply.github.com>
2026-08-18 15:33:45 +0200

    security(auth): deactivate legacy demo accounts

---
+++ backend/apps/accounts/migrations/0009_deactivate_legacy_test_users.py
@@ -0,0 +1,22 @@
+from django.db import migrations
+
+
+LEGACY_TEST_EMAILS = [
+    "testadmin@maputopublicidade.co.mz",
+    "cliente@maputopublicidade.co.mz",
+]
+
+
+def deactivate_legacy_test_users(apps, schema_editor):
+    User = apps.get_model("auth", "User")
+    User.objects.filter(email__in=LEGACY_TEST_EMAILS).update(is_active=False)
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("accounts", "0008_provision_etios_admin"),
+    ]
+
+    operations = [
+        migrations.RunPython(deactivate_legacy_test_users, migrations.RunPython.noop),
+    ]
